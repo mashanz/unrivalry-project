c067779a745eccaf2c0e5ad093fb62e2cbc91358
mashanz <mangatkk@gmail.com>
2020-09-04 16:42:14 +0000

    add settings

--- unrivalry/core/settings.py
+++ unrivalry/core/settings.py
@@ -20,7 +20,7 @@ DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',
-                 config('SERVER', default='127.0.0.1')]
+                 config('SERVER', default='127.0.0.1'), 'unrivalry.com']
 
 # Application definition
 
