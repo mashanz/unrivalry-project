3db520fef63d1a947cfb066821d5f6b750faf52c
Mashanz <mangatkk@gmail.com>
2020-09-07 10:25:27 +0700

    fixing error pages and footer

--- unrivalry/unrivalryWeb/views.py
+++ unrivalry/unrivalryWeb/views.py
@@ -11,3 +11,20 @@ import datetime
 class WebViews():
     def index(request):
         return render(request, "web/index.html")
+
+    def pages(request):
+        context = {}
+        # All resource paths end in .html.
+        # Pick out the html file name from the url. And load that template.
+        try:
+            load_template = request.path.split('web/')[-1]
+            html_template = loader.get_template(load_template)
+            return HttpResponse(html_template.render(context, request))
+
+        except template.TemplateDoesNotExist:
+            html_template = loader.get_template('web/page-404.html')
+            return HttpResponse(html_template.render(context, request))
+
+        except:
+            html_template = loader.get_template('web/page-500.html')
+            return HttpResponse(html_template.render(context, request))
