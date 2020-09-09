829d14b46fa324d134f797d2c96393a6b15b4ded
Mashanz <mangatkk@gmail.com>
2020-09-09 23:24:42 +0700

    Update settings.py

--- unrivalry/core/settings.py
+++ unrivalry/core/settings.py
@@ -8,6 +8,8 @@ from decouple import config
 from unipath import Path
 import dj_database_url
 
+SECURE_SSL_REDIRECT = True
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = Path(__file__).parent
