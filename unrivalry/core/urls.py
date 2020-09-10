f9ca2ed1de668643955bb46343002eaaaf33db62
Mashanz <mangatkk@gmail.com>
2020-09-10 15:15:57 +0700

    Ignores and delete unused

--- unrivalry/core/urls.py
+++ unrivalry/core/urls.py
@@ -6,8 +6,10 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     # Custom App URL
     path("dashboard", include("unrivalryAdmin.urls")),
     path("dashboard/", include("unrivalryAdmin.urls")),
@@ -22,6 +24,8 @@ urlpatterns = [
     path('devadmin/', admin.site.urls),           # Django admin route
     path('accounts/', include('allauth.urls')),
     path("", include("authentication.urls")),  # Auth routes - login / register
+
+    # Everything Else under this is unknown or 404
     path("", include("unrivalryWeb.urls")),
     # path("", include("app.urls")),             # UI Kits Html files
 ]
--- unrivalry/unrivalryWeb/admin.py
+++ unrivalry/unrivalryWeb/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(MusicForDistribution)
+admin.site.register(Artist)
+admin.site.register(MusicGenre)
+admin.site.register(MusicSubGenre)
