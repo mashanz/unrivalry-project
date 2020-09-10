0cc6c84c3e57610d978465912c10b0b52dc9d8be
Mashanz <mangatkk@gmail.com>
2020-09-10 10:03:38 +0700

    Social Login

--- unrivalry/core/settings.py
+++ unrivalry/core/settings.py
@@ -33,11 +33,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'app',  # Enable the inner app
     'unrivalryWeb',
     'unrivalryAdmin',
     'unrivalryApi',
     'portfolio',
+    'socialLogin',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +145,22 @@ STATICFILES_DIRS = (
 )
 #############################################################
 #############################################################
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 2
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
--- unrivalry/core/urls.py
+++ unrivalry/core/urls.py
@@ -5,6 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # Custom App URL
@@ -16,8 +17,10 @@ urlpatterns = [
     path("portfolio/", include("portfolio.urls")),
 
     # Core App URL
-    # path('admin', admin.site.urls),            # Django admin route
-    # path('admin/', admin.site.urls),           # Django admin route
+    path('soclog/', TemplateView.as_view(template_name="soclog/index.html")),
+    path('admin', admin.site.urls),            # Django admin route
+    path('admin/', admin.site.urls),           # Django admin route
+    path('accounts/', include('allauth.urls')),
     path("", include("authentication.urls")),  # Auth routes - login / register
     path("", include("unrivalryWeb.urls")),
     # path("", include("app.urls")),             # UI Kits Html files
