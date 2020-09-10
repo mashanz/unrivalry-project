131aa3b70bda3e50a721bc4b9990aff6389ec0f7
Mashanz <mangatkk@gmail.com>
2020-09-10 16:40:42 +0700

    Static files change

--- unrivalry/core/settings.py
+++ unrivalry/core/settings.py
@@ -148,8 +148,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media')
+if str(socket.gethostname()) == 'localhost.local':
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, '..', '..', '..', 'www', 'media')
 MEDIA_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
