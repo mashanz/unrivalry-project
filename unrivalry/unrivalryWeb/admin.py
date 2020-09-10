4932ed876c4478429a40fbe0a09b28ec81a65c03
Mashanz <mangatkk@gmail.com>
2020-09-10 15:27:12 +0700

    delete models

--- unrivalry/unrivalryWeb/admin.py
+++ unrivalry/unrivalryWeb/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
 admin.site.register(MusicForDistribution)
 admin.site.register(Artist)
 admin.site.register(MusicGenre)
--- unrivalry/unrivalryWeb/models.py
+++ unrivalry/unrivalryWeb/models.py
@@ -4,26 +4,6 @@ from django.utils import timezone
 # Create your models here.
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
-
-
 class Artist(models.Model):
     foto = models.ImageField(upload_to="artist")
     name = models.CharField(max_length=200)
@@ -47,7 +27,7 @@ class MusicSubGenre(models.Model):
 
 
 class MusicForDistribution(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, default=True)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Tahun Release')
     genre = models.ForeignKey(MusicGenre, on_delete=models.CASCADE, null=True)
