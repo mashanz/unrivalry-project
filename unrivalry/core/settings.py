b2aa66f939c4e1113571f0aab38f78c95f80d619
Mashanz <mangatkk@gmail.com>
2020-09-10 13:35:09 +0700

    Update settings.py

--- unrivalry/core/settings.py
+++ unrivalry/core/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'core.urls'
-LOGIN_REDIRECT_URL = "home"   # Route defined in app/urls.py
+LOGIN_REDIRECT_URL = "/dashboard"   # Route defined in app/urls.py
 LOGOUT_REDIRECT_URL = "home"  # Route defined in app/urls.py
 TEMPLATE_DIR = os.path.join(
     BASE_DIR, "core/templates")  # ROOT dir for templates
