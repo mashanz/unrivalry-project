566f3a2246dfc5b885a0397b8f323fc5aae3c0fa
Mashanz <mangatkk@gmail.com>
2020-09-10 16:18:18 +0700

    fix media roots

--- unrivalry/core/settings.py
+++ unrivalry/core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False)
-DEBUG = True
+# DEBUG = True
 
 # load production server from .env
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',
@@ -149,13 +149,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'core/static'),
-    os.path.join(BASE_DIR, 'media'),
 )
 #############################################################
 #############################################################
